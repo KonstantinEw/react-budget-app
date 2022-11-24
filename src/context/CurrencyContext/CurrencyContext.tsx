ec3ef5a380f80fb10170bbdde4795822fa383e15
KonstantinEw <Ewwel@inbox.ru>
2022-11-24 10:55:14 +0300

    add(PROJ-4):  add hook useForm() and fix problem

--- src/context/CurrencyContext/CurrencyContext.tsx
+++ src/context/CurrencyContext/CurrencyContext.tsx
@@ -1,3 +1,42 @@
-import React, { createContext } from "react";
+import { createContext, ReactNode, useState } from "react";
+import { Currency } from "../../config/currency";
+interface ICurrencyOption {
+  label: keyof typeof Currency;
+  value: Currency;
+}
 
-export const CurrencyContext = createContext({});
+interface ICurrencyContext {
+  currentCurrency: ICurrencyOption;
+  currencies: ICurrencyOption[];
+  setCurrency: (option: ICurrencyOption) => void;
+}
+
+const options: ICurrencyOption[] = [
+  { value: Currency.USD, label: "USD" },
+  { value: Currency.EUR, label: "EUR" },
+  { value: Currency.GBR, label: "GBR" },
+];
+
+export const CurrencyContext = createContext<ICurrencyContext>({} as ICurrencyContext);
+
+export const useCurrencyContextValue = () => {
+  const [currencyContextValue, setCurrencyContextValue] = useState<ICurrencyContext>(() => ({
+    currentCurrency: options[0],
+    currencies: options,
+    setCurrency: (option) => {
+      setCurrencyContextValue((ctx) => ({
+        ...ctx,
+        currentCurrency: option,
+      }));
+    },
+  }));
+  return currencyContextValue;
+};
+
+export const CurrencyContextProvider = ({ children }: { children: ReactNode }) => {
+  return (
+    <CurrencyContext.Provider value={useCurrencyContextValue()}>
+      {children}
+    </CurrencyContext.Provider>
+  );
+};
